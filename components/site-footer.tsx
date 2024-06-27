d362ed20f8c315e32c4e7277ce723006ac33e1d4
KushwahaVikashJ <kushwahavicky743@gmail.com>
2024-06-27 10:41:25 +0530

    updated components

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -1,19 +1,13 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import { LogoDarkPixl, SelloDarkPixl } from "./Footer/SelloDarkpixl"
 import { Flag } from "./Icons/flags"
 import { FigmaIcon } from "lucide-react"
 
 export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className={cn(className)}>
-      <div className="flex items-center justify-center">
-        <div className="relative flex">
-          <SelloDarkPixl className="animate-spin-slow" />
-          <LogoDarkPixl />
-        </div>
-      </div>
+      <div className="flex items-center justify-center"></div>
 
       <div className="mb-32 mt-8 flex flex-col items-center justify-center gap-2.5">
         <div className="flex h-9 w-64 items-center justify-center gap-2 rounded-lg border border-slate-800 bg-gradient-to-b from-transparent to-black/20 text-xs font-medium text-neutral-100 text-opacity-80 shadow">
